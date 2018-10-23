f1f0935199be1cc70035926d29a6eccb0f7c8ebc
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2018-10-23 17:38:29 -0400

    Add task to build k8s template

---
+++ src/tasks/build-k8s-vm-template.ts
@@ -0,0 +1,254 @@
+/**
+ * @module root.tasks.buildBaselineTemplate
+ */
+import _loggerProvider from '@vamship/logger';
+import { SshClient } from '@vamship/ssh-utils';
+import { Promise } from 'bluebird';
+import Listr from 'listr';
+import { IRemoteHostInfo, ITaskDefinition } from '../types';
+
+const cloneBaselineTemplateCommands = [
+    ['# Copy the baseline template', 'qm clone 1000 1001 --name k8s-node'].join(
+        '\n'
+    ),
+
+    [
+        '# Configure the template with ip address and ssh key',
+        [
+            'qm set 1001 --ciuser kube --sshkey ~/.ssh/id_rsa_template.pub',
+            '--ipconfig0 ip=10.0.0.11/24,gw=10.0.0.1 --nameserver 8.8.8.8'
+        ].join(' ')
+    ].join('\n'),
+
+    ['# Resize disk', 'qm resize 1001 scsi0 +8G'].join('\n'),
+
+    ['# Start an instance of the template', 'qm start 1001'].join('\n')
+];
+
+const installSoftwareCommands = [
+    [
+        '# Install docker, kubectl, kubeadm and kubelet',
+        "ssh -i ~/.ssh/id_rsa_template kube@10.0.0.11 <<'END_SCRIPT'",
+        "sudo su <<'END_SUDO'",
+        '',
+        '# Echo commands',
+        'set -x',
+        '',
+        '# Update APT, install dependencies',
+        'apt-get update',
+        'apt-get install -y apt-transport-https ca-certificates curl software-properties-common',
+        '',
+        '# Install docker',
+        'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -',
+        [
+            'add-apt-repository',
+            '"deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")',
+            '$(lsb_release -cs) stable"'
+        ].join(' '),
+        'apt-get update',
+        [
+            'apt-get install -y docker-ce=$(apt-cache madison docker-ce',
+            "| grep 18.06 | head -1 | awk '{print $3}')"
+        ].join(' '),
+        '',
+        '# Install kubectl, kubeadm and kubelet',
+        'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -',
+        'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list',
+        'deb http://apt.kubernetes.io/ kubernetes-xenial main',
+        'EOF',
+        'apt-get update',
+        'apt-get install -y kubelet kubeadm kubectl',
+        'apt-mark hold kubelet kubeadm kubectl',
+        '',
+        'END_SUDO',
+        'END_SCRIPT'
+    ].join('\n')
+];
+
+const cleanupTemplateCommands = [
+    [
+        '# Clean up instance and prep for conversion to template',
+        "ssh -i ~/.ssh/id_rsa_template kube@10.0.0.11 <<'END_SCRIPT'",
+        "sudo su <<'END_SUDO'",
+        '',
+        '# Echo commands',
+        'set -x',
+        '',
+        '# Clean up apt cache files',
+        'apt clean all',
+        '',
+        '# Clean up log files',
+        'logrotate -f /etc/logrotate.conf',
+        'rm -f /var/log/*.gz /var/log/*.1',
+        '',
+        'rm -f /var/apt/*.xz /var/apt/*.gz',
+        'rm -rf /var/log/containers/*',
+        'rm -rf /var/log/dist-upgrade/*',
+        'rm -rf /var/log/lxd/*',
+        'rm -rf /var/log/landscape/*',
+        'rm -rf /var/log/journal/*',
+        'rm -rf /var/log/pods/*',
+        'rm -rf /var/log/unattended-upgrades/*',
+        '',
+        'cat /dev/null > /var/log/alternatives.log',
+        'cat /dev/null > /var/log/auth.log',
+        'cat /dev/null > /var/log/btmp',
+        'cat /dev/null > /var/log/cloud-init-output.log',
+        'cat /dev/null > /var/log/dpkg.log',
+        'cat /dev/null > /var/log/kern.log',
+        'cat /dev/null > /var/log/lastlog',
+        'cat /dev/null > /var/log/syslog',
+        'cat /dev/null > /var/log/tallylog',
+        'cat /dev/null > /var/log/wtmp',
+        '',
+        '# Clean up temp files',
+        'rm -rf /tmp/*',
+        'rm -rf /var/tmp/*',
+        '',
+        '# Clean up SSH keys',
+        'rm -f /etc/ssh/*key*',
+        '',
+        '# Clean up bash history and SSH keys for the user: kube.',
+        'rm -f ~kube/.bash_history',
+        'rm -rf ~kube/.ssh/',
+        'rm -rf ~kube/.cache/',
+        'rm -rf ~kube/.gnupg/',
+        'rm -f ~kube/sudo_as_admin_successful',
+        '',
+        '# Clean up bash history and SSH keys for the user: root.',
+        'rm -f ~root/.bash_history',
+        'rm -rf ~root/.ssh/',
+        'unset HISTFILE',
+        '',
+        'END_SUDO',
+        'END_SCRIPT'
+    ].join('\n'),
+
+    ['# Shutdown the instance', 'qm shutdown 1001'].join('\n'),
+
+    [
+        '# Reset ssh keys and ip configuration for the template',
+        'qm set 1001 --sshkeys ./nokey --ipconfig0 ip=dhcp'
+    ].join('\n')
+];
+
+const convertToTemplateCommands = [
+    ['# Convert the VM into a template', 'qm template 1001'].join('\n')
+];
+
+/**
+ * Returns a task that can be used to build a template that will serve as
+ * as the template for all k8s nodes.
+ *
+ * @param hostInfo Informtation about the remote host against which the task
+ *        will be executed.
+ *
+ * @return ITaskDefinition A task definition that can be used to execute the
+ *         task.
+ */
+export const getTask = (hostInfo: IRemoteHostInfo): ITaskDefinition => {
+    return {
+        title: 'Build k8s VM template',
+        task: () => {
+            const logger = _loggerProvider.getLogger('build-k8s-vm-template');
+            return new Listr([
+                {
+                    title: 'Clone and configure baseline template',
+                    task: () => {
+                        logger.trace('Clone and configure baseline template');
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(cloneBaselineTemplateCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    const err = new Error(
+                                        'Error cloning baseline into k8s VM'
+                                    );
+                                    logger.error(err);
+                                    throw err;
+                                }
+                                logger.debug(
+                                    'Cloned baseline template into k8s VM'
+                                );
+                            });
+                    }
+                },
+                {
+                    title: 'Wait for VM instance to start up',
+                    task: () => {
+                        return Promise.delay(180000);
+                    }
+                },
+                {
+                    title: 'Install docker, kubectl, kubeadm and kubelet',
+                    task: () => {
+                        logger.trace(
+                            'Clean up template; prep for conversion to template'
+                        );
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(installSoftwareCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    const err = new Error(
+                                        'Error installing required software on VM'
+                                    );
+                                    logger.error(err);
+                                    throw err;
+                                }
+                                logger.debug(
+                                    'Required software installed on VM'
+                                );
+                            });
+                    }
+                },
+                {
+                    title: 'Clean up template; prep for conversion to template',
+                    task: () => {
+                        logger.trace(
+                            'Clean up template; prep for conversion to template'
+                        );
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(cleanupTemplateCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    const err = new Error(
+                                        'Error cleaning up VM instance'
+                                    );
+                                    logger.error(err);
+                                    throw err;
+                                }
+                                logger.debug(
+                                    'VM instance cleaned up for conversion to template'
+                                );
+                            });
+                    }
+                },
+                {
+                    title: 'Convert VM into template',
+                    task: () => {
+                        logger.trace('Convert VM into template');
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(convertToTemplateCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    const err = new Error(
+                                        'Error converting VM into template'
+                                    );
+                                    logger.error(err);
+                                    throw err;
+                                }
+                                logger.debug('Converted VM into template');
+                            });
+                    }
+                }
+            ]);
+        }
+    };
+};
---
+++ test/unit/tasks/build-k8s-vm-template-spec.ts
@@ -0,0 +1,188 @@
+import { default as _chai, expect } from 'chai';
+import 'mocha';
+import _sinon from 'sinon';
+
+import {
+    asyncHelper as _asyncHelper,
+    ObjectMock,
+    testValues as _testValues
+} from '@vamship/test-utils';
+import _rewire from 'rewire';
+import { injectSshSubTaskSuite } from '../../utils/test-suite-helper';
+
+const _taskModule = _rewire('../../../src/tasks/build-k8s-vm-template');
+
+describe('[build-k8s-vm-template task]', () => {
+    function _getTaskDefinition(args: object = {}) {
+        const host = _testValues.getString('host');
+        const port = _testValues.getNumber(100, 22);
+        const username = _testValues.getString('username');
+        const privateKey = _testValues.getString('privateKey');
+        const password = _testValues.getString('password');
+
+        const hostInfo = Object.assign(
+            {
+                host,
+                port,
+                username,
+                privateKey,
+                password
+            },
+            args
+        );
+
+        return _taskModule.getTask(hostInfo);
+    }
+
+    let _listrMock;
+    let _sshClientMock;
+    let _promiseMock;
+    const VM_STARTUP_WAIT_TIME = 180000;
+
+    beforeEach(() => {
+        _listrMock = new ObjectMock().addPromiseMock('run');
+        _sshClientMock = new ObjectMock().addPromiseMock('run');
+        _promiseMock = new ObjectMock().addMock('delay', () => {
+            return _promiseMock.__delayPromise;
+        });
+        _promiseMock.__delayPromise = {
+            then: () => true
+        };
+
+        _taskModule.__set__('listr_1', {
+            default: _listrMock.ctor
+        });
+        _taskModule.__set__('ssh_utils_1', {
+            SshClient: _sshClientMock.ctor
+        });
+        _taskModule.__set__('bluebird_1', {
+            Promise: _promiseMock.instance
+        });
+    });
+
+    describe('[init]', () => {
+        it('should export expected properties', () => {
+            expect(_taskModule.getTask).to.be.a('function');
+        });
+    });
+
+    describe('getTask()', () => {
+        const expectedTitle = 'Build k8s VM template';
+        const subTaskList = [
+            {
+                title: 'Clone and configure baseline template',
+                commandCount: 4,
+                eatError: false
+            },
+            {
+                title: 'Wait for VM instance to start up',
+                commandCount: -1,
+                nonSshTask: true,
+                eatError: false
+            },
+            {
+                title: 'Install docker, kubectl, kubeadm and kubelet',
+                commandCount: 1,
+                eatError: false
+            },
+            {
+                title: 'Clean up template; prep for conversion to template',
+                commandCount: 3,
+                eatError: false
+            },
+            {
+                title: 'Convert VM into template',
+                commandCount: 1,
+                eatError: false
+            }
+        ];
+
+        function _getSubTaskRunner(taskIndex: number) {
+            return (
+                args: object = {},
+                ctx: object = {},
+                task: object = {}
+            ): Promise<undefined> => {
+                _listrMock.ctor.resetHistory();
+                _getTaskDefinition(args).task();
+                return _listrMock.ctor.args[0][0][taskIndex].task(ctx, task);
+            };
+        }
+
+        it('should return a task definition when invoked', () => {
+            const taskDefinition = _getTaskDefinition();
+
+            expect(taskDefinition.title).to.equal(expectedTitle);
+            expect(taskDefinition.task).to.be.a('function');
+        });
+
+        it('should create and return a Listr object when the task is invoked', () => {
+            expect(_listrMock.ctor).to.not.have.been.called;
+
+            const ret = _getTaskDefinition().task();
+
+            expect(_listrMock.ctor).to.have.been.calledOnce;
+            expect(ret).to.equal(_listrMock.instance);
+        });
+
+        it('should define the expected number of sub tasks with correct titles', () => {
+            _getTaskDefinition().task();
+            expect(_listrMock.ctor.args[0]).to.have.length(1);
+
+            const subTaskArg = _listrMock.ctor.args[0][0];
+            expect(subTaskArg).to.be.an('array');
+
+            const subTaskTitles = subTaskList.map((task) => task.title);
+            expect(subTaskArg).to.have.length(subTaskTitles.length);
+
+            subTaskTitles.forEach((title, index) => {
+                expect(subTaskArg[index]).to.be.an('object');
+                expect(subTaskArg[index].title).to.equal(title);
+                expect(subTaskArg[index].task).to.be.a('function');
+            });
+        });
+
+        subTaskList.forEach(
+            ({ title, commandCount, eatError, nonSshTask }, index) => {
+                describe(`[sub task: ${title}]`, () => {
+                    const execSubTask = _getSubTaskRunner(index);
+                    const getSshClientMock = () => _sshClientMock;
+
+                    if (!nonSshTask) {
+                        injectSshSubTaskSuite(
+                            commandCount,
+                            eatError,
+                            execSubTask,
+                            getSshClientMock
+                        );
+                    } else {
+                        it('should return a promise when invoked', () => {
+                            const ret = execSubTask();
+
+                            expect(ret).to.be.an('object');
+                            expect(ret.then).to.be.a('function');
+                        });
+
+                        it('should invoke the delay method to introduce a delay', () => {
+                            const delayMethod = _promiseMock.mocks.delay;
+
+                            expect(delayMethod.stub).to.not.have.been.called;
+
+                            execSubTask();
+
+                            expect(delayMethod.stub).to.have.been.calledOnce;
+                            expect(delayMethod.stub).to.have.been.calledWith(
+                                VM_STARTUP_WAIT_TIME
+                            );
+                        });
+
+                        it('should return the promise from the delay method', () => {
+                            const ret = execSubTask();
+                            expect(ret).to.equal(_promiseMock.__delayPromise);
+                        });
+                    }
+                });
+            }
+        );
+    });
+});
