5dbbf1a971bb3d15303cea824bf445999ec1a4f7
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2018-10-23 11:36:49 -0400

    Add task to configure dhcp server

---
+++ src/tasks/configure-dhcp.ts
@@ -0,0 +1,180 @@
+/**
+ * @module root.tasks.configureDhcp
+ */
+import _loggerProvider from '@vamship/logger';
+import { SshClient } from '@vamship/ssh-utils';
+import Listr from 'listr';
+import { IRemoteHostInfo, ITaskDefinition } from '../types';
+
+const checkConfigRequiredCommands = [
+    'grep -iq "vmbr300" /etc/default/isc-dhcp-server'
+];
+const installDhcpServerCommands = [
+    'apt update',
+    'apt install -y isc-dhcp-server'
+];
+const configureDhcpServerCommands = [
+    [
+        "cat <<'EOF' >> /etc/default/isc-dhcp-server",
+        'INTERFACESv4="vmbr300"',
+        'INTERFACESv6=""',
+        '',
+        'DHCPDv4_CONF=/etc/dhcp/dhcpd.conf',
+        '#DHCPDv6_CONF=/etc/dhcp/dhcpd6.conf',
+        'EOF'
+    ].join('\n')
+];
+const configureDhcpDaemonCommands = [
+    [
+        "cat <<'EOF' >> /etc/dhcp/dhcpd.conf",
+        'default-lease-time          3600;',
+        'max-lease-time              7200;',
+        '',
+        'subnet 10.0.0.0 netmask 255.255.255.0 {',
+        '    range 10.0.0.128 10.0.0.254;',
+        '    option routers              10.0.0.1;',
+        '    option subnet-mask          255.255.255.0;',
+        '    option broadcast-address    10.0.0.255;',
+        '    option domain-name-servers  8.8.8.8;',
+        '}',
+        'EOF'
+    ].join('\n')
+];
+const restartDhcpServiceCommands = [
+    'systemctl restart isc-dhcp-server.service'
+];
+
+/**
+ * Returns a task that can be used to configure a DHCP server for the host.
+ * This task works by installing isc-dhcp-server and writing configuration
+ * data into `/etc/default/isc-dhcp-server` and to `/etc/dhcp/dhcpd.conf`.
+ * A preliminary check is made to ensure that the configuration update is
+ * necessary, but the test is fairly basic - it ensures that no duplicate
+ * records are written, but cannot correct existing config if it is partial or
+ * incorrect.
+ *
+ * @param hostInfo Informtation about the remote host against which the task
+ *        will be executed.
+ *
+ * @return ITaskDefinition A task definition that can be used to execute the
+ *         task.
+ */
+export const getTask = (hostInfo: IRemoteHostInfo): ITaskDefinition => {
+    return {
+        title: 'Configure DHCP',
+        task: () => {
+            const logger = _loggerProvider.getLogger('configure-nat');
+            function skip(ctx) {
+                if (ctx.skipDhcpConfig) {
+                    logger.warn('Skipping NAT configuration');
+                    return 'NAT already configured';
+                }
+                logger.debug('NAT configuration required');
+                return false;
+            }
+
+            return new Listr([
+                {
+                    title: 'Check if DHCP configuration is required',
+                    task: (ctx, task) => {
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(checkConfigRequiredCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    logger.debug('DHCP configuration required');
+                                    ctx.skipDhcpConfig = false;
+                                } else {
+                                    logger.warn('DHCP already configured');
+                                    ctx.skipDhcpConfig = true;
+                                }
+                            });
+                    }
+                },
+                {
+                    title: 'Install DHCP server',
+                    skip,
+                    task: () => {
+                        logger.trace('Install DHCP server');
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(installDhcpServerCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    const err = new Error(
+                                        'Error installing DHCP server'
+                                    );
+                                    logger.error(err);
+                                    throw err;
+                                }
+                                logger.debug('DHCP server installed');
+                            });
+                    }
+                },
+                {
+                    title: 'Configure DHCP server defaults',
+                    skip,
+                    task: () => {
+                        logger.trace('Configure DHCP server defaults');
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(configureDhcpServerCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    const err = new Error(
+                                        'Error configuring DHCP server defaults'
+                                    );
+                                    logger.error(err);
+                                    throw err;
+                                }
+                                logger.debug('DHCP server configured');
+                            });
+                    }
+                },
+                {
+                    title: 'Configure DHCP daemon',
+                    skip,
+                    task: () => {
+                        logger.trace('Configure DHCP daemon');
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(configureDhcpDaemonCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    const err = new Error(
+                                        'Error configuring DHCP daemon'
+                                    );
+                                    logger.error(err);
+                                    throw err;
+                                }
+                                logger.debug('DHCP daemon configured');
+                            });
+                    }
+                },
+                {
+                    title: 'Restart DHCP service',
+                    skip,
+                    task: () => {
+                        const sshClient = new SshClient(hostInfo);
+                        return sshClient
+                            .run(restartDhcpServiceCommands)
+                            .then((results) => {
+                                logger.trace(results);
+                                if (results.failureCount > 0) {
+                                    logger.warn(
+                                        'DHCP service restart returned an error. Ignoring.'
+                                    );
+                                } else {
+                                    logger.debug('DHCP service restarted');
+                                }
+                            });
+                    }
+                }
+            ]);
+        }
+    };
+};
---
+++ test/unit/tasks/configure-dhcp-spec.ts
@@ -0,0 +1,214 @@
+import { default as _chai, expect } from 'chai';
+import 'mocha';
+import _sinon from 'sinon';
+
+import { ObjectMock, testValues as _testValues } from '@vamship/test-utils';
+import _rewire from 'rewire';
+import { injectSshSubTaskSuite } from '../../utils/test-suite-helper';
+
+const _taskModule = _rewire('../../../src/tasks/configure-dhcp');
+
+describe('[configure-dhcp task]', () => {
+    function _getTaskDefinition(args: object = {}) {
+        const host = _testValues.getString('host');
+        const port = _testValues.getNumber(100, 22);
+        const username = _testValues.getString('username');
+        const privateKey = _testValues.getString('privateKey');
+        const password = _testValues.getString('password');
+
+        const hostInfo = Object.assign(
+            {
+                host,
+                port,
+                username,
+                privateKey,
+                password
+            },
+            args
+        );
+
+        return _taskModule.getTask(hostInfo);
+    }
+
+    let _listrMock;
+    let _sshClientMock;
+
+    beforeEach(() => {
+        _listrMock = new ObjectMock().addPromiseMock('run');
+        _sshClientMock = new ObjectMock().addPromiseMock('run');
+
+        _taskModule.__set__('listr_1', {
+            default: _listrMock.ctor
+        });
+        _taskModule.__set__('ssh_utils_1', {
+            SshClient: _sshClientMock.ctor
+        });
+    });
+
+    describe('[init]', () => {
+        it('should export expected properties', () => {
+            expect(_taskModule.getTask).to.be.a('function');
+        });
+    });
+
+    describe('getTask()', () => {
+        const expectedTitle = 'Configure DHCP';
+        const subTaskList = [
+            {
+                title: 'Check if DHCP configuration is required',
+                commandCount: 1,
+                eatError: true
+            },
+            {
+                title: 'Install DHCP server',
+                commandCount: 2,
+                eatError: false
+            },
+            {
+                title: 'Configure DHCP server defaults',
+                commandCount: 1,
+                eatError: false
+            },
+            {
+                title: 'Configure DHCP daemon',
+                commandCount: 1,
+                eatError: false
+            },
+            {
+                title: 'Restart DHCP service',
+                commandCount: 1,
+                eatError: true
+            }
+        ];
+
+        function _getSubTaskRunner(taskIndex: number) {
+            return (
+                args: object = {},
+                ctx: object = {},
+                task: object = {}
+            ): Promise<undefined> => {
+                _listrMock.ctor.resetHistory();
+                _getTaskDefinition(args).task();
+                return _listrMock.ctor.args[0][0][taskIndex].task(ctx, task);
+            };
+        }
+
+        it('should return a task definition when invoked', () => {
+            const taskDefinition = _getTaskDefinition();
+
+            expect(taskDefinition.title).to.equal(expectedTitle);
+            expect(taskDefinition.task).to.be.a('function');
+        });
+
+        it('should create and return a Listr object when the task is invoked', () => {
+            expect(_listrMock.ctor).to.not.have.been.called;
+
+            const ret = _getTaskDefinition().task();
+
+            expect(_listrMock.ctor).to.have.been.calledOnce;
+            expect(ret).to.equal(_listrMock.instance);
+        });
+
+        it('should define the expected number of sub tasks with correct titles', () => {
+            _getTaskDefinition().task();
+            expect(_listrMock.ctor.args[0]).to.have.length(1);
+
+            const subTaskArg = _listrMock.ctor.args[0][0];
+            expect(subTaskArg).to.be.an('array');
+
+            const subTaskTitles = subTaskList.map((task) => task.title);
+            expect(subTaskArg).to.have.length(subTaskTitles.length);
+
+            subTaskTitles.forEach((title, index) => {
+                expect(subTaskArg[index]).to.be.an('object');
+                expect(subTaskArg[index].title).to.equal(title);
+                expect(subTaskArg[index].task).to.be.a('function');
+            });
+        });
+
+        subTaskList.forEach(({ title, commandCount, eatError }, index) => {
+            describe(`[sub task: ${title}]`, () => {
+                const execSubTask = _getSubTaskRunner(index);
+                const getSshClientMock = () => _sshClientMock;
+
+                injectSshSubTaskSuite(
+                    commandCount,
+                    eatError,
+                    execSubTask,
+                    getSshClientMock
+                );
+
+                if (index === 0) {
+                    it('should set the ctx.skipDhcpConfig=false if command execution fails', () => {
+                        const sshClientMock = getSshClientMock();
+                        const runMethod = sshClientMock.mocks.run;
+                        const ctx = {
+                            skipDhcpConfig: undefined
+                        };
+
+                        const ret = execSubTask(undefined, ctx);
+                        runMethod.resolve({
+                            commandCount,
+                            successCount: 0,
+                            failureCount: commandCount
+                        });
+
+                        return expect(ret).to.be.fulfilled.then(() => {
+                            expect(ctx.skipDhcpConfig).to.be.false;
+                        });
+                    });
+
+                    it('should set the ctx.skipDhcpConfig=true if command execution succeeds', () => {
+                        const sshClientMock = getSshClientMock();
+                        const runMethod = sshClientMock.mocks.run;
+                        const ctx = {
+                            skipDhcpConfig: undefined
+                        };
+
+                        const ret = execSubTask(undefined, ctx);
+                        runMethod.resolve({
+                            commandCount,
+                            successCount: commandCount,
+                            failureCount: 0
+                        });
+
+                        return expect(ret).to.be.fulfilled.then(() => {
+                            expect(ctx.skipDhcpConfig).to.be.true;
+                        });
+                    });
+                } else {
+                    describe('[skip]', () => {
+                        function _execSkip(
+                            args: object = {},
+                            ctx: object = {}
+                        ) {
+                            _listrMock.ctor.resetHistory();
+                            _getTaskDefinition(args).task();
+                            return _listrMock.ctor.args[0][0][index].skip(ctx);
+                        }
+
+                        it('should define a skip function', () => {
+                            _getTaskDefinition({}).task();
+                            const skip = _listrMock.ctor.args[0][0][index].skip;
+                            expect(skip).to.be.a('function');
+                        });
+
+                        it('should return false if ctx.skipDhcpConfig === false', () => {
+                            const ret = _execSkip(undefined, {
+                                skipDhcpConfig: false
+                            });
+                            expect(ret).to.be.false;
+                        });
+
+                        it('should return a message if ctx.skipDhcpConfig === true', () => {
+                            const ret = _execSkip(undefined, {
+                                skipDhcpConfig: true
+                            });
+                            expect(ret).to.equal('NAT already configured');
+                        });
+                    });
+                }
+            });
+        });
+    });
+});
